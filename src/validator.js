746d83c75ac628a54646b2de024d0fd3054d7121
bmolteni <brunomolteni90@gmail.com>
2016-08-20 14:56:26 -0300

    validator.js passing all tests

--- src/validator.js
+++ src/validator.js
@@ -2,49 +2,66 @@
 
 const _ = require('lodash');
 
-function throwNotImplemented() {
-    throw new Error('Not implemented');
+const helper = require('../test/helper.js');
+
+// helper function that returns a sorted array with the ammount of repeated values in a hand
+function valuesCount(hand){
+  let handCards = helper.cards(hand);
+  let valuesArr =  _.map( handCards , hand => hand.split('').shift() );
+
+  return  _(valuesArr).countBy().values().value().sort( (a,b) => a-b );
+}
+
+// helper function that returns a string of all the values in the hand, ordered and with the ace in the correct position
+function orderedValues(hand){
+  let handCards = helper.cards(hand);
+  let orderedValues = _(handCards).map( hand => hand.split('').shift() ).sortBy(number => '234567890JQKA'.indexOf(number)).value();
+  // we move the ace to the beggining if it may work as a straight
+  if(_(orderedValues).head() === '2' && _(orderedValues).last() === 'A' ) orderedValues.unshift( orderedValues.pop() );
+
+  return _(orderedValues).sum();
 }
 
 function straightFlush(hand) {
-    throwNotImplemented();
+    return flush(hand) && _.includes( 'A234567890JQKA' , orderedValues(hand) );
 }
 
 function fourOfAKind(hand) {
-    throwNotImplemented();
+    return _.includes( valuesCount(hand) , 4 );
 }
 
 function fullHouse(hand) {
-    throwNotImplemented();
+    return _.isEqual( valuesCount(hand) , [2,3]);
 }
 
 function flush(hand) {
-    throwNotImplemented();
+    let handCards = helper.cards(hand);
+    return _(handCards).map( hand => hand.split('').pop() ).uniq().value().length === 1;
 }
 
 function straight(hand) {
-    throwNotImplemented();
+    return !flush(hand) && _.includes( 'A234567890JQKA' , orderedValues(hand) );
 }
 
 function threeOfAKind(hand) {
-    throwNotImplemented();
+    return _.isEqual( valuesCount(hand) , [1,1,3] );
 }
 
 function twoPair(hand) {
-    throwNotImplemented();
+    return _.isEqual( valuesCount(hand) , [1,2,2]);
 }
 
 function onePair(hand) {
-    throwNotImplemented();
+    return _.isEqual( valuesCount(hand) , [1,1,1,2] );
 }
 
 module.exports = {
-    // straightFlush,
-    // fourOfAKind,
-    // fullHouse,
-    // flush,
-    // straight,
-    // threeOfAKind,
-    // twoPair,
-    // onePair
+    straightFlush,
+    fourOfAKind,
+    fullHouse,
+    flush,
+    straight,
+    threeOfAKind,
+    twoPair,
+    onePair
 };
